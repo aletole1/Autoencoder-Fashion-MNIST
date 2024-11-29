7d23edd03c7e7ca3cab0f7e3cfa80ae28b72afca
aletoledo <ale.tole.454@gmail.com>
2024-11-29 13:58:12 -0300

    Punto 1 y 2 trabajo final

---
+++ Trainer.py
@@ -0,0 +1,56 @@
+import torch
+from tqdm import tqdm
+
+class Trainer:
+    def __init__(self, model, train_dataloader, test_dataloader, loss_fn, optimizer, device, epochs=10):
+        self.model = model
+        self.train_dataloader = train_dataloader
+        self.test_dataloader = test_dataloader
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.device = device
+        self.epochs = epochs
+        self.list_train_losses = []
+        self.list_test_train_set_losses = []
+        self.list_test_eval_set_losses = []
+
+    def train_loop(self):
+        self.model.train()
+        running_loss = 0
+        for (X, _) in self.train_dataloader:
+            X = X.to(self.device)
+            output = self.model(X)
+            loss = self.loss_fn(output, X)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            
+            running_loss += loss.item()
+        return running_loss / len(self.train_dataloader)
+
+    def eval_loop(self, dataloader):
+        self.model.eval()
+        running_loss = 0
+        num_batches = len(dataloader)
+
+        with torch.no_grad():
+            for X, _ in dataloader:
+                X = X.to(self.device)
+                pred = self.model(X)
+                loss = self.loss_fn(pred, X)
+
+                running_loss += loss.item()
+
+        avg_loss = running_loss / num_batches
+        return avg_loss
+
+    def train_model(self):
+        for _ in tqdm(range(self.epochs)):
+            train_loss = self.train_loop()
+            test_train_set_loss = self.eval_loop(self.train_dataloader)
+            test_eval_set_loss = self.eval_loop(self.test_dataloader)
+
+            self.list_train_losses.append(train_loss)
+            self.list_test_train_set_losses.append(test_train_set_loss)
+            self.list_test_eval_set_losses.append(test_eval_set_loss)
+        return self.list_train_losses, self.list_test_train_set_losses, self.list_test_eval_set_losses
---
+++ main.py
@@ -0,0 +1,117 @@
+import Autoencoder
+import os
+import json
+import torch
+from torchvision import datasets, transforms
+from CustomDataset import CustomDataset
+from Trainer import Trainer
+
+def train_configuration(config, t_dataset, v_dataset):
+    """ 
+    Entrena y valida un autoencoder con los datos de t_dataset y v_dataset
+
+    La función recibe un diccionario con la configuración del autoencoder de la forma:
+    {
+        "learning_rate": float,
+        "dropout": float,
+        "batch_size": int,
+        "epochs": int,
+        "lineal": bool True si el modelo tiene capa lienal intermedia, False c.c. 
+    } 
+    """
+    lr = config["learning_rate"]
+    dropout = config["dropout"]
+    batch_size = config["batch_size"]
+    epochs = config["epochs"]
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # Se crea el autoencoder
+    if config["S2F"]:
+        model = Autoencoder.Autoencoder_S2F(dropout)
+    else:
+        model = Autoencoder.Autoencoder_F2S(dropout)
+
+    # Se envía el modelo al dispositivo
+    model.to(device)
+
+    # Se crea el optimizador y la función de pérdida
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    loss_fn = torch.nn.MSELoss()
+
+    # Se crea el DataLoader para los datos de entrenamiento y validación
+    train_loader = torch.utils.data.DataLoader(t_dataset, batch_size=batch_size, shuffle=True, num_workers=os.cpu_count()-1)
+    valid_loader = torch.utils.data.DataLoader(v_dataset, batch_size=batch_size, shuffle=True, num_workers=os.cpu_count()-1)
+
+    # Se entrena el modelo
+    trainer = Trainer(model, train_loader, valid_loader, loss_fn, optimizer, device, epochs)
+    train_loss_incorrect, train_loss, valid_loss = trainer.train_model()
+
+    return model, train_loss_incorrect, train_loss, valid_loss
+
+
+def train_all(configs, t_dataset, v_dataset):
+    """
+    Entrena y valida todos los autoencoders con las configuraciones en configs
+
+    La función recibe una lista de diccionarios con las configuraciones de los autoencoders de la forma:
+    [
+        {
+            "learning_rate": float,
+            "dropout": float,
+            "batch_size": int,
+            "epochs": int,
+            "lineal": bool True si el modelo tiene capa lienal intermedia, False c.c. 
+        },
+        ...
+    ] 
+
+    Retorna una lista de tuplas con la forma:
+    [
+        (modelo_1, train_loss_incorrect_1, train_loss_1, valid_loss_1),
+        (modelo_2, train_loss_incorrect_2, train_loss_2, valid_loss_2),
+        ...
+    ]
+    """
+    results = {}
+    for i,(config) in enumerate(configs):
+        model, train_loss_incorrect, train_loss, valid_loss = train_configuration(config, t_dataset, v_dataset)
+        results = {
+            "config_"+str(i): config,
+            "model_"+str(i): model,
+            "train_loss_incorrect_"+str(i): train_loss_incorrect,
+            "train_loss_"+str(i): train_loss,
+            "valid_loss_"+str(i): valid_loss
+        }
+        with open(f'./results/result_{i}.json', 'w') as f:
+            json.dump(results, f)
+            
+
+configurations = [
+    { # configuracion default
+        "learning_rate": 0.001,
+        "dropout": 0.2,
+        "batch_size": 100,
+        "epochs": 50,
+        "S2F": True
+    },
+    {
+        "learning_rate": 0.001,
+        "dropout": 0.2,
+        "batch_size": 100,
+        "epochs": 50,
+        "S2F": False
+    },
+]
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,), (0.5,))])
+
+# Se cargan los datasets
+train_set_orig = datasets.FashionMNIST('MNIST_data/', download = True, train = True,  transform = transform)
+valid_set_orig = datasets.FashionMNIST('MNIST_data/', download = True, train = False, transform = transform)
+
+train_set_autoencoder = CustomDataset(train_set_orig)
+valid_set_autoencoder = CustomDataset(valid_set_orig)
+
+train_all(configurations, train_set_autoencoder, valid_set_autoencoder)
